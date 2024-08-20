eed0f920eb38e037f66c6da2080c37667e83f3ed
Nizar Septian <nizar47@gmail.com>
2024-08-20 17:42:51 -0400

    Update SelectMCThrown a.Cnd ReweightMCRec.C

--- macros/ReweightMCRec.C
+++ macros/ReweightMCRec.C
@@ -1,33 +1,30 @@
 // Description: Reweight MC reco to match data t slope
 
-const Int_t BeamEnergyBin = 0;
-
-TString inputDir = "/d/home/septian/Eta3PiDalitz/run/root4AmptoolsEnergyDep/";
-TString fileInputName = Form("eta_2017_data_t01506_ccdbFlux_EgBin_genEtaRegge_%d.root",BeamEnergyBin);
-TString fileInputNameMC = Form("mc_rec_2017_data_t01506_ccdbFlux_EgBin_genEtaRegge_%d.root",BeamEnergyBin);
-
-TString outputDir = "/d/home/septian/Eta3PiDalitz/run/root4AmptoolsEnergyDep/";
-TString fileOutputName = Form("reweighted_mc_rec_2017_data_t01506_ccdbFlux_EgBin_genEtaRegge_%d.root",BeamEnergyBin);
-
 TString pdfOutDir = "/d/home/septian/Eta3PiDalitzPlots/";
-TString pdfOut_base = Form("2017_data_t01506_ccdbFlux_EgBin_genEtaRegge_%d",BeamEnergyBin);
-
-Bool_t isGenerateWeightedTrees = kTRUE;
 
 void gluex_style();
-void ReweightMCRec(){
+void ReweightMCRec(Int_t data_set, TString tag, Int_t PhotonBeamEnergyBinIdx, Bool_t isGenerateWeightedTrees=kTRUE) {
 
-    gluex_style();
-	gROOT->ForceStyle();
+    TString runPeriod[3] = {"2017","2018S","2018F"};
+    TString workingDir = Form("/d/home/septian/Eta3PiDalitz/run/Eta3PiSelectorParallelTree_%s/",tag.Data());
+    TString fileInputName = Form("data_%s_%s_%d.root",runPeriod[data_set].Data(),tag.Data(),PhotonBeamEnergyBinIdx);
+    TString fileInputNameMC = Form("mc_rec_%s_%s_%d.root",runPeriod[data_set].Data(),tag.Data(),PhotonBeamEnergyBinIdx);
+    TString fileOutputName = Form("reweighted_mc_rec_%s_%s_%d.root",runPeriod[data_set].Data(),tag.Data(),PhotonBeamEnergyBinIdx);
+
+    TString pdfOut_base = Form("%s_%s_%d",runPeriod[data_set].Data(),tag.Data(),PhotonBeamEnergyBinIdx);
+
+    gStyle->SetOptStat(0);
+    
     //open input file
-    TFile *fileInput = new TFile(inputDir+fileInputName);
-    TFile *fileInputMC = new TFile(inputDir+fileInputNameMC);
+    TFile *fileInput = new TFile(workingDir+fileInputName);
+    TFile *fileInputMC = new TFile(workingDir+fileInputNameMC);
 
-    TDirectory *dir = (TDirectory*)fileInput->Get("hist_SidebandSub");
-    TDirectory *dirMC = (TDirectory*)fileInputMC->Get("hist_SidebandSub");
+    TString dirName = "Histograms";
+    TDirectory *dir = (TDirectory*)fileInput->Get(dirName.Data());
+    TDirectory *dirMC = (TDirectory*)fileInputMC->Get(dirName.Data());
 
     //get mandelstam_t histograms
-    TString hname = "h_mandelstam_t_sbsAll";
+    TString hname = "h1_MandelstamT";
     TH1F *h1_mandelstam_t_data = (TH1F*)dir->Get(hname.Data());
     TH1F *h1_mandelstam_t_MC = (TH1F*)dirMC->Get(hname.Data());
     
@@ -41,13 +38,14 @@ void ReweightMCRec(){
     h1_mandelstam_t_data->SetStats(0);
     h1_mandelstam_t_data->SetLineColor(kRed);
     h1_mandelstam_t_data->SetLineWidth(2);
+    h1_mandelstam_t_MC->GetXaxis()->SetRangeUser(0.15,0.6);
     h1_mandelstam_t_MC->SetLineColor(kBlue);
     h1_mandelstam_t_MC->SetLineWidth(2);
     h1_mandelstam_t_MC->SetTitle("Mandelstam t");
 
     TCanvas *c1 = new TCanvas("c1","c1",800,600);
-    h1_mandelstam_t_data->Draw();
-    h1_mandelstam_t_MC->Draw("SAME");
+    h1_mandelstam_t_MC->Draw();
+    h1_mandelstam_t_data->Draw("SAME");
     TLegend *leg1 = new TLegend(0.6,0.7,0.85,0.85);
     leg1->AddEntry(h1_mandelstam_t_data,"Data","l");
     leg1->AddEntry(h1_mandelstam_t_MC,"MC","l");
@@ -59,10 +57,10 @@ void ReweightMCRec(){
     TH1F *h1_mandelstam_t_ratio = (TH1F*)h1_mandelstam_t_data->Clone();
     h1_mandelstam_t_ratio->Divide(h1_mandelstam_t_MC);
     h1_mandelstam_t_ratio->SetTitle("Mandelstam t ratio");
-    h1_mandelstam_t_ratio->GetXaxis()->SetTitle("Mandelstam t [GeV^{2}]");
-    h1_mandelstam_t_ratio->GetYaxis()->SetTitle("Ratio");
+    h1_mandelstam_t_ratio->GetXaxis()->SetTitle("|t| [GeV^{2}]");
+    h1_mandelstam_t_ratio->GetYaxis()->SetTitle("Ratio [Data/MC]");
     h1_mandelstam_t_ratio->SetStats(0);
-    h1_mandelstam_t_ratio->SetLineColor(kRed);
+    h1_mandelstam_t_ratio->SetLineColor(kBlack);
     h1_mandelstam_t_ratio->SetLineWidth(2);
 
     //fit ratio with polynomial function and print as pdf
@@ -76,7 +74,7 @@ void ReweightMCRec(){
     c1->SaveAs((pdfOutDir+pdfOut_base).Data()+TString("_mandelstam_t_ratio.pdf"));
 
     //create output file
-    TFile *fileOutput = new TFile(outputDir+fileOutputName,"RECREATE");
+    TFile *fileOutput = new TFile(workingDir+fileOutputName,"RECREATE");
 
     // h1_mandelstam_t_data->Write();
     // h1_mandelstam_t_MC->Write();
@@ -90,6 +88,7 @@ void ReweightMCRec(){
         TTree *dataChain = (TTree*)fileInputMC->Get("nt");
         
         Double_t EnBeam,PxBeam,PyBeam,PzBeam;
+        Double_t EnProton,PxProton,PyProton,PzProton;
         Double_t EnP1,PxP1,PyP1,PzP1;
         Double_t EnP2,PxP2,PyP2,PzP2;
         Double_t EnP3,PxP3,PyP3,PzP3;
@@ -99,6 +98,11 @@ void ReweightMCRec(){
         dataChain->SetBranchAddress("PxBeam",&PxBeam);
         dataChain->SetBranchAddress("PyBeam",&PyBeam);
         dataChain->SetBranchAddress("PzBeam",&PzBeam);
+
+        dataChain->SetBranchAddress("EnProton",&EnProton);
+        dataChain->SetBranchAddress("PxProton",&PxProton);
+        dataChain->SetBranchAddress("PyProton",&PyProton);
+        dataChain->SetBranchAddress("PzProton",&PzProton);
 
         dataChain->SetBranchAddress("EnP1",&EnP1);
         dataChain->SetBranchAddress("PxP1",&PxP1);
@@ -120,26 +124,22 @@ void ReweightMCRec(){
         TTree *dataChainWeighted = dataChain->CloneTree(0);
 
         //t histogram for unweighted events
-        TH1F *h1_mandelstam_t_unweighted = new TH1F("h1_mandelstam_t_unweighted","Unweighted mandelstam t",100,0.15,0.6);
+        TH1F *h1_mandelstam_t_unweighted = new TH1F("h1_mandelstam_t_unweighted","Unweighted mandelstam t",101,0.15,0.6);
         h1_mandelstam_t_unweighted->GetXaxis()->SetTitle("|t| [GeV^{2}]");
         h1_mandelstam_t_unweighted->GetYaxis()->SetTitle("Events");
 
         //t histogram for weighted events
-        TH1F *h1_mandelstam_t_weighted = new TH1F("h1_mandelstam_t_weighted","Weighted mandelstam t",100,0.15,0.6);
+        TH1F *h1_mandelstam_t_weighted = new TH1F("h1_mandelstam_t_weighted","Weighted mandelstam t",101,0.15,0.6);
         h1_mandelstam_t_weighted->GetXaxis()->SetTitle("|t| [GeV^{2}]");
         h1_mandelstam_t_weighted->GetYaxis()->SetTitle("Events");
 
         for (Int_t i=0; i<dataChain->GetEntries(); i++) {
             dataChain->GetEntry(i);
-            TLorentzVector P1,P2,P3;
-            P1.SetPxPyPzE(PxP1,PyP1,PzP1,EnP1);
-            P2.SetPxPyPzE(PxP2,PyP2,PzP2,EnP2);
-            P3.SetPxPyPzE(PxP3,PyP3,PzP3,EnP3);
-            TLorentzVector P4Eta = P1+P2+P3;
+            TLorentzVector P4_RecoilProton = TLorentzVector(PxProton,PyProton,PzProton,EnProton);
+            TLorentzVector P4_TargetProton = TLorentzVector(0,0,0,0.938272);
 
-            TLorentzVector P4Beam = TLorentzVector(PxBeam,PyBeam,PzBeam,EnBeam);
-
-            Double_t mandelstam_t = -1*(P4Beam-P4Eta).M2();
+            Double_t mandelstam_t = -1*(P4_TargetProton-P4_RecoilProton).M2();
+            // cout << mandelstam_t << endl;
             h1_mandelstam_t_unweighted->Fill(mandelstam_t,weight);
 
             weight *= fit_mandelstam_t_ratio->Eval(mandelstam_t);
@@ -151,7 +151,7 @@ void ReweightMCRec(){
         h1_mandelstam_t_unweighted->Write();
         h1_mandelstam_t_weighted->Write();
         h1_mandelstam_t_weighted->SetMinimum(0);
-        h1_mandelstam_t_weighted->SetLineColor(kRed);
+        h1_mandelstam_t_weighted->SetLineColor(kBlack);
         h1_mandelstam_t_weighted->SetTitle("Weighted and unweighted mandelstam t");
         h1_mandelstam_t_weighted->Draw("");
         // h1_mandelstam_t_unweighted->SetMarkerStyle(2);
@@ -162,52 +162,22 @@ void ReweightMCRec(){
         leg3->AddEntry(h1_mandelstam_t_weighted,"Weighted","l");
         leg3->Draw();
         c1->SaveAs((pdfOutDir+pdfOut_base).Data()+TString("_mandelstam_t_weighted.pdf"));
+
+        // draw data with scaled reweighted MC
+
+        // cout << h1_mandelstam_t_weighted->Integral() << " " << h1_mandelstam_t_data->Integral() << endl;
+        h1_mandelstam_t_weighted->Scale(h1_mandelstam_t_data->Integral()/h1_mandelstam_t_weighted->Integral());
+        c1->Clear();
+        h1_mandelstam_t_weighted->GetXaxis()->SetRangeUser(0.15,0.6);
+        h1_mandelstam_t_weighted->SetLineWidth(2);
+        h1_mandelstam_t_weighted->Draw();
+        h1_mandelstam_t_data->Draw("SAME");
+        leg1->Clear();
+        leg1->AddEntry(h1_mandelstam_t_data,"Data","l");
+        leg1->AddEntry(h1_mandelstam_t_weighted,"Reweighted MC","l");
+        leg1->Draw();
+        c1->SaveAs((pdfOutDir+pdfOut_base).Data()+TString("_mandelstam_t_data_reweightedMC.pdf"));
     }
     fileOutput->Close();
 
-}
-
-void gluex_style() {
-	///////// 
-	// Make the plots prettier with bigger fonts
-	// Original code is https://halldweb.jlab.org/wiki-private/index.php/Guidance_for_Presentations 
-	// 	slightly modified
-	/////////
-	auto gluex_style = new TStyle("GlueX","Default GlueX Style");
-
-        gluex_style->SetCanvasBorderMode(0);
-        gluex_style->SetPadBorderMode(0);
-        gluex_style->SetPadColor(0);
-        gluex_style->SetCanvasColor(0);
-        gluex_style->SetStatColor(0);
-
-	// some default window sizes
-	gluex_style->SetCanvasDefW(800);
-	gluex_style->SetCanvasDefH(600);
-
-	// let's change the default margins
-	gluex_style->SetPadBottomMargin(0.2);
-	gluex_style->SetPadLeftMargin(0.2);
-	gluex_style->SetPadTopMargin(0.08);
-	gluex_style->SetPadRightMargin(0.15);
-
-	// axis labels and settings
-    gluex_style->SetStripDecimals(0);
- 	gluex_style->SetLabelSize(0.045,"xyz"); // size of axis value font
- 	gluex_style->SetTitleSize(0.06,"xyz"); // size of axis title font
- 	gluex_style->SetTitleFont(42,"xyz"); // font option
- 	gluex_style->SetLabelFont(42,"xyz"); 
- 	gluex_style->SetTitleOffset(1.5,"y"); 
- 	gluex_style->SetLabelOffset(0.01,"xyz");   // stop collisions of "0"s at the origin
- 	
-	// histogram settings
-	gluex_style->SetOptStat(0);     // no stats box by default
-	gluex_style->SetOptTitle(1);    // no title by default
-	gluex_style->SetHistLineWidth(2); 
-	gluex_style->SetNdivisions(508,"xyz"); // some ticks were very bunched, lets reduce the number of divisions to label 
-	// gluex_style->SetOptFit(0111);
-
-	gluex_style->SetHistFillColor(920);   // grey
-	gluex_style->SetPalette(kViridis); // kViridis is perceptually uniform and colorblind friendly
-	gluex_style->cd();
 }
--- macros/selectMCThrown.C
+++ macros/selectMCThrown.C
@@ -1,21 +1,33 @@
-const Double_t PhotonBeamEnergyBin[6] = {6.5,7.5,8.0,9.0,10.0,11.6};
-const Int_t NEnergyBin=5;
-// const Double_t PhotonBeamEnergyBin[NEnergyBin+1] = {6.5,11.6};
+// const Double_t PhotonBeamEnergyBin[6] = {6.5,7.5,8.0,9.0,10.0,11.6};
+// const Double_t PhotonBeamEnergyBin[2] = {6.5,11.6};
+const Double_t MandelstamtTRange[2] = {0.15,0.6};
+// const Int_t NEnergyBin=3;
+const Double_t PhotonBeamEnergyBin[4] = {3.0,4.0,5.0,6.5};
 // const Double_t MandelstamtTRange[2] = {0.1,0.6};
 // TString fileInputName = "/d/home/septian/Eta3PiDalitz/run/root4Amptools/Eta3Pi_Thrown_2017_genEtaRegge_flat.root";
 // TString fileInputName = "/d/grid17/septian/Eta3PiDalitz/DSelectors/Eta3Pi_Thrown_2017_genEtaRegge_flat.root";
 // TString tag = "t0106_ccdbFlux_oldMC";
+
+// const Double_t PhotonBeamEnergyBin[NEnergyBin+1] = {3.0,11.6};
 TString outDir = "/d/home/septian/Eta3PiDalitzPlots/";
 
-void selectMCThrown(TString fileInputName, TString tag, Int_t PhotonBeamEnergyIdx=0, Double_t tMin=0.15, Double_t tMax=0.6){ 
+void selectMCThrown(Int_t data_set, TString tag, Int_t PhotonBeamEnergyIdx){ 
     gStyle->SetOptStat(0);
-    TString fileOutputBaseName = Form("root4AmptoolsEnergyDep/mc_thrown_2017_%s_",tag.Data());
-    const Double_t MandelstamtTRange[2] = {tMin,tMax};
+    TString runPeriod[3] = {"2017","2018S","2018F"};
+    TString workingDir = Form("/d/home/septian/Eta3PiDalitz/run/Eta3PiSelectorParallelTree_t01506_ccdbFlux_genEtaRegge/");
+    TString DSelectorDir = "/d/home/septian/Eta3PiDalitz/DSelectors/";
+    // TString fileInputName[3] = {Form("%sEta3Pi_Thrown_2017_ccdbFlux_genEtaRegge_flat.root",DSelectorDir.Data()),
+    //                             Form("%sTree_Eta3Pi_Thrown_2018S_ccdbFlux_genEtaRegge_flat.root",DSelectorDir.Data()),
+    //                             ""}; 
+    TString fileInputName[3] = {Form("%sEta3Pi_Thrown_2017_cobremsFlux_genEtaRegge_flat.root",DSelectorDir.Data()),
+                                "",
+                                ""}; 
+    TString fileOutputBaseName = Form("Eta3PiSelectorParallelTree_%s/mc_thrown_%s_%s_%d",tag.Data(),runPeriod[data_set].Data(),tag.Data(),PhotonBeamEnergyIdx);
     
     TChain *dataChain = new TChain("nt");
-    dataChain->Add(fileInputName.Data());
+    dataChain->Add(fileInputName[data_set].Data());
     Int_t nEntries = dataChain->GetEntries();
-    cout << "Filename: " << fileInputName << endl; 
+    cout << "Filename: " << fileInputName[data_set] << endl; 
     cout << "Number of entries: " << nEntries << endl;
 
     Double_t EnP1,PxP1,PyP1,PzP1;
@@ -45,108 +57,94 @@ void selectMCThrown(TString fileInputName, TString tag, Int_t PhotonBeamEnergyId
     dataChain->SetBranchAddress("t",&t);
     dataChain->SetBranchAddress("EnBeam",&EnBeam);
 
-    //loop over energy bins
     TCanvas *c = new TCanvas("c","c",800,600);
     TH1F *h1 = new TH1F("h1","h1",50,MandelstamtTRange[0],MandelstamtTRange[1]);
     TH1F *h1_pippimg1g2mass = new TH1F("h1_pippimg1g2mass","h1_pippimg1g2mass",100,0.45,0.65);
     TH2F *h2_DalitzPlot = new TH2F("h2_DalitzPlotEta3Pi_thrown","h2_DalitzPlotEta3Pi_thrown",101,-1.0,1.0,101,-1.0,1.0);
-    for(Int_t i=0;i<NEnergyBin;i++){
-        Double_t EnBeamMin = PhotonBeamEnergyBin[i];
-        Double_t EnBeamMax = PhotonBeamEnergyBin[i+1];
-        cout << "EnBeamMin: " << EnBeamMin << endl;
-        cout << "EnBeamMax: " << EnBeamMax << endl;
+    
+    Double_t EnBeamMin = PhotonBeamEnergyBin[PhotonBeamEnergyIdx];
+    Double_t EnBeamMax = PhotonBeamEnergyBin[PhotonBeamEnergyIdx+1];
+    cout << "EnBeamMin: " << EnBeamMin << endl;
+    cout << "EnBeamMax: " << EnBeamMax << endl;
 
-        TTree *treeOutput = new TTree("nt","nt");
-        
-        //create branches
-        treeOutput->Branch("EnP1",&EnP1,"EnP1/D");
-        treeOutput->Branch("PxP1",&PxP1,"PxP1/D");
-        treeOutput->Branch("PyP1",&PyP1,"PyP1/D");
-        treeOutput->Branch("PzP1",&PzP1,"PzP1/D");
-        treeOutput->Branch("EnP2",&EnP2,"EnP2/D");
-        treeOutput->Branch("PxP2",&PxP2,"PxP2/D");
-        treeOutput->Branch("PyP2",&PyP2,"PyP2/D");
-        treeOutput->Branch("PzP2",&PzP2,"PzP2/D");
-        treeOutput->Branch("EnP3",&EnP3,"EnP3/D");
-        treeOutput->Branch("PxP3",&PxP3,"PxP3/D");
-        treeOutput->Branch("PyP3",&PyP3,"PyP3/D");
-        treeOutput->Branch("PzP3",&PzP3,"PzP3/D");
-        Double_t Weight = 1;
-        treeOutput->Branch("weight",&Weight,"weight/D");            
-
-        //loop over entries
-
-        for(Int_t i=0;i<nEntries;i++){
-            dataChain->GetEntry(i);
-            if(EnBeam<EnBeamMin || EnBeam>EnBeamMax) continue;
-            if(TMath::Abs(t)<MandelstamtTRange[0] || TMath::Abs(t)>MandelstamtTRange[1]) continue;
-
-            // calculate X and Y
-            TLorentzVector P4_PiPlus,P4_PiMinus,P4_PiZero;
-            P4_PiPlus.SetPxPyPzE(PxP1,PyP1,PzP1,EnP1);
-            P4_PiMinus.SetPxPyPzE(PxP2,PyP2,PzP2,EnP2);
-            P4_PiZero.SetPxPyPzE(PxP3,PyP3,PzP3,EnP3);
-
-            Double_t etaMass = (P4_PiPlus + P4_PiMinus + P4_PiZero).M();
-
-            TLorentzVector P4_Eta = P4_PiPlus + P4_PiMinus + P4_PiZero;
-        
-            P4_PiPlus.Boost(-P4_Eta.BoostVector());
-            P4_PiMinus.Boost(-P4_Eta.BoostVector());
-            P4_PiZero.Boost(-P4_Eta.BoostVector());
-
-            Double_t T_PiPlus = P4_PiPlus.E() - P4_PiPlus.M();
-            Double_t T_PiMinus = P4_PiMinus.E() - P4_PiMinus.M();
-            Double_t T_PiZero = P4_PiZero.E() - P4_PiZero.M();
-
-            Double_t Dalitz_X = TMath::Sqrt(3.0)*(T_PiPlus - T_PiMinus)/(T_PiPlus + T_PiMinus + T_PiZero);
-            Double_t Dalitz_Y = 3.0*T_PiZero/(T_PiPlus + T_PiMinus + T_PiZero) - 1.0;
-
-            // fill output tree
-            treeOutput->Fill();
-
-            // fill histogram
-            h1->Fill(TMath::Abs(t),weight);
-            h1_pippimg1g2mass->Fill(etaMass,weight);
-            h2_DalitzPlot->Fill(Dalitz_X,Dalitz_Y,Weight);
-        }
-        //write output file
-        TString fileOutputName = Form("%s%d.root",fileOutputBaseName.Data(),i);
-        cout << "Output file: " << fileOutputName << endl;
-        //create output file
-        TFile *fileOutput = new TFile(fileOutputName.Data(),"RECREATE");
-        treeOutput->Write();
-        treeOutput->Delete();
-
-        h1->GetXaxis()->SetTitle("|t| (GeV^{2})");
-        h1->GetYaxis()->SetTitle("Events");
-        h1->SetTitle(Form("Mandelstam-t distribution for %0.2f < E_{#gamma} < %0.2f GeV",EnBeamMin,EnBeamMax));
-        // fit h1 with exponential model
-        TF1 *f1 = new TF1("f1","[0]*exp([1]*x)",MandelstamtTRange[0],MandelstamtTRange[1]);
-        h1->Fit("f1","R");
-        // show fit parameters on the plot
-        TLatex Tl;
-        Tl.SetTextAlign(12);
-        Tl.SetTextSize(0.03);
-        h1->Draw("E");
-        Tl.DrawLatexNDC(0.15,0.30,Form("#chi^{2}/ndf = %.2f",f1->GetChisquare()/f1->GetNDF()));
-        Tl.DrawLatexNDC(0.15,0.25,Form("p0 = %.2f #pm %.2f",f1->GetParameter(0),f1->GetParError(0)));
-        Tl.DrawLatexNDC(0.15,0.20,Form("p1 = %.2f #pm %.2f",f1->GetParameter(1),f1->GetParError(1)));
-        c->SaveAs(Form("%sh_mandelstam_t_%s_%0.2f_%0.2f.pdf",outDir.Data(),tag.Data(),EnBeamMin,EnBeamMax));
-        h1->Write();
-
-        h2_DalitzPlot->GetXaxis()->SetTitle("Dalitz X");
-        h2_DalitzPlot->GetYaxis()->SetTitle("Dalitz Y");
-        h2_DalitzPlot->Draw("COLZ");
-        c->SaveAs(Form("%sh_DalitzPlotEta3Pi_thrown_%s_%0.2f_%0.2f.pdf",outDir.Data(),tag.Data(),EnBeamMin,EnBeamMax));
-        h2_DalitzPlot->Write();
-
-        h1_pippimg1g2mass->GetXaxis()->SetTitle("M_{#pi^{+}#pi^{-}#pi^{0}} (GeV)");
-        h1_pippimg1g2mass->GetYaxis()->SetTitle("Events");
-        h1_pippimg1g2mass->SetTitle(Form("Thrown M_{#pi^{+}#pi^{-}#pi^{0}} distribution for %0.2f < E_{#gamma} < %0.2f GeV",EnBeamMin,EnBeamMax));
-        h1_pippimg1g2mass->Draw("hist");
-        c->SaveAs(Form("%sh_pippimg1g2mass_%s_%0.2f_%0.2f.pdf",outDir.Data(),tag.Data(),EnBeamMin,EnBeamMax));
-
-        fileOutput->Close();
+    TTree *treeOutput = new TTree("nt","nt");
+    
+    //create branches
+    treeOutput->Branch("EnP1",&EnP1,"EnP1/D");
+    treeOutput->Branch("PxP1",&PxP1,"PxP1/D");
+    treeOutput->Branch("PyP1",&PyP1,"PyP1/D");
+    treeOutput->Branch("PzP1",&PzP1,"PzP1/D");
+    treeOutput->Branch("EnP2",&EnP2,"EnP2/D");
+    treeOutput->Branch("PxP2",&PxP2,"PxP2/D");
+    treeOutput->Branch("PyP2",&PyP2,"PyP2/D");
+    treeOutput->Branch("PzP2",&PzP2,"PzP2/D");
+    treeOutput->Branch("EnP3",&EnP3,"EnP3/D");
+    treeOutput->Branch("PxP3",&PxP3,"PxP3/D");
+    treeOutput->Branch("PyP3",&PyP3,"PyP3/D");
+    treeOutput->Branch("PzP3",&PzP3,"PzP3/D");
+    treeOutput->Branch("EnBeam",&EnBeam,"EnBeam/D");
+    Double_t Weight = 1;
+    treeOutput->Branch("weight",&Weight,"weight/D");            
+    //loop over entries
+    for(Int_t i=0;i<nEntries;i++){
+        dataChain->GetEntry(i);
+        if(EnBeam<EnBeamMin || EnBeam>EnBeamMax) continue;
+        if(TMath::Abs(t)<MandelstamtTRange[0] || TMath::Abs(t)>MandelstamtTRange[1]) continue;
+        // calculate X and Y
+        TLorentzVector P4_PiPlus,P4_PiMinus,P4_PiZero;
+        P4_PiPlus.SetPxPyPzE(PxP1,PyP1,PzP1,EnP1);
+        P4_PiMinus.SetPxPyPzE(PxP2,PyP2,PzP2,EnP2);
+        P4_PiZero.SetPxPyPzE(PxP3,PyP3,PzP3,EnP3);
+        Double_t etaMass = (P4_PiPlus + P4_PiMinus + P4_PiZero).M();
+        TLorentzVector P4_Eta = P4_PiPlus + P4_PiMinus + P4_PiZero;
+    
+        P4_PiPlus.Boost(-P4_Eta.BoostVector());
+        P4_PiMinus.Boost(-P4_Eta.BoostVector());
+        P4_PiZero.Boost(-P4_Eta.BoostVector());
+        Double_t T_PiPlus = P4_PiPlus.E() - P4_PiPlus.M();
+        Double_t T_PiMinus = P4_PiMinus.E() - P4_PiMinus.M();
+        Double_t T_PiZero = P4_PiZero.E() - P4_PiZero.M();
+        Double_t Dalitz_X = TMath::Sqrt(3.0)*(T_PiPlus - T_PiMinus)/(T_PiPlus + T_PiMinus + T_PiZero);
+        Double_t Dalitz_Y = 3.0*T_PiZero/(T_PiPlus + T_PiMinus + T_PiZero) - 1.0;
+        // fill output tree
+        treeOutput->Fill();
+        // fill histogram
+        h1->Fill(TMath::Abs(t),weight);
+        h1_pippimg1g2mass->Fill(etaMass,weight);
+        h2_DalitzPlot->Fill(Dalitz_X,Dalitz_Y,Weight);
     }
+    //write output file
+    TString fileOutputName = Form("%s.root",fileOutputBaseName.Data());
+    cout << "Output file: " << fileOutputName << endl;
+    //create output file
+    TFile *fileOutput = new TFile(fileOutputName.Data(),"RECREATE");
+    treeOutput->Write();
+    treeOutput->Delete();
+    h1->GetXaxis()->SetTitle("|t| (GeV^{2})");
+    h1->GetYaxis()->SetTitle("Events");
+    h1->SetTitle(Form("Mandelstam-t distribution for %0.2f < E_{#gamma} < %0.2f GeV",EnBeamMin,EnBeamMax));
+    // fit h1 with exponential model
+    TF1 *f1 = new TF1("f1","[0]*exp([1]*x)",MandelstamtTRange[0],MandelstamtTRange[1]);
+    h1->Fit("f1","R");
+    // show fit parameters on the plot
+    TLatex Tl;
+    Tl.SetTextAlign(12);
+    Tl.SetTextSize(0.03);
+    h1->Draw("E");
+    Tl.DrawLatexNDC(0.15,0.30,Form("#chi^{2}/ndf = %.2f",f1->GetChisquare()/f1->GetNDF()));
+    Tl.DrawLatexNDC(0.15,0.25,Form("p0 = %.2f #pm %.2f",f1->GetParameter(0),f1->GetParError(0)));
+    Tl.DrawLatexNDC(0.15,0.20,Form("p1 = %.2f #pm %.2f",f1->GetParameter(1),f1->GetParError(1)));
+    c->SaveAs(Form("%sh_mandelstam_t_%s_%0.2f_%0.2f.pdf",outDir.Data(),tag.Data(),EnBeamMin,EnBeamMax));
+    h1->Write();
+    h2_DalitzPlot->GetXaxis()->SetTitle("Dalitz X");
+    h2_DalitzPlot->GetYaxis()->SetTitle("Dalitz Y");
+    h2_DalitzPlot->Draw("COLZ");
+    c->SaveAs(Form("%sh_DalitzPlotEta3Pi_thrown_%s_%0.2f_%0.2f.pdf",outDir.Data(),tag.Data(),EnBeamMin,EnBeamMax));
+    h2_DalitzPlot->Write();
+    h1_pippimg1g2mass->GetXaxis()->SetTitle("M_{#pi^{+}#pi^{-}#pi^{0}} (GeV)");
+    h1_pippimg1g2mass->GetYaxis()->SetTitle("Events");
+    h1_pippimg1g2mass->SetTitle(Form("Thrown M_{#pi^{+}#pi^{-}#pi^{0}} distribution for %0.2f < E_{#gamma} < %0.2f GeV",EnBeamMin,EnBeamMax));
+    h1_pippimg1g2mass->Draw("hist");
+    c->SaveAs(Form("%sh_pippimg1g2mass_%s_%0.2f_%0.2f.pdf",outDir.Data(),tag.Data(),EnBeamMin,EnBeamMax));
+    fileOutput->Close();
 }
